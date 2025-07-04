be76f1d057cd166267113260972483f013612b9e
msi <lg.mobile0808@gmail.com>
2025-07-04 20:55:04 +0300

    Solution

--- taxi/admin.py
+++ taxi/admin.py
@@ -3,40 +3,32 @@ from django.contrib.auth.admin import UserAdmin
 from .models import Manufacturer, Car, Driver
 
 
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Manufacturer, Car, Driver
+
+
 @admin.register(Manufacturer)
 class ManufacturerAdmin(admin.ModelAdmin):
-    list_display = ('name', 'country')
-    search_fields = ('name',)
+    list_display = ("name", "country")
+    search_fields = ("name",)
+    list_filter = ("country",)
 
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
-    list_display = ('model', 'manufacturer')
-    search_fields = ('model',)
-    list_filter = ('manufacturer',)
-    filter_horizontal = ('drivers',)
+    list_display = ("model", "manufacturer")
+    search_fields = ("model",)
+    list_filter = ("manufacturer",)
+    filter_horizontal = ("drivers",)
 
 
 @admin.register(Driver)
 class DriverAdmin(UserAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'license_number')
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
-    search_fields = ('username', 'first_name', 'last_name', 'email', 'license_number')
-
-    fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
-        ('Additional info', {'fields': ('license_number',)}),  # Це критично важливо
-        ('Permissions', {
-            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
-        }),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    list_display = UserAdmin.list_display + ("license_number",)
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("license_number",)}),
     )
-
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'password1', 'password2', 'email',
-                       'first_name', 'last_name', 'license_number'),
-        }),
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("license_number",)}),
     )
